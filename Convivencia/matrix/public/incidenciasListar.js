d7c3d493b286cb45604e06e2d59361a933ddc27e
DawYorch <yorch100976@gmail.com>
2018-11-29 17:46:58 +0100

    Cambio

--- Convivencia/matrix/public/incidenciasListar.js
+++ Convivencia/matrix/public/incidenciasListar.js
@@ -5,7 +5,8 @@ $.getJSON( "incidencias", function( data ) {
     // fila = document.createElement("tr");
     // columna_nombre
     // items.push(fila);
-    items.push( "<li id='" + key + "' class='list-group-item'> Alumno: " + val.alumno + " Profesor : "+val.profe+" Tipo de Incidencia "+val.gravedad+"</li>" );
+    items.push( "<li id='" + key + "' class='list-group-item'> ALUMNO: " + val.alumno + " PROFESOR : "+val.profe+
+    " TIPO: "+val.gravedad+"<a href="+"http://localhost:3000/Convivencia2.html?analid="+val._id+"> Ver Incidencia </a></li>" );
   });
  
   $( "<ul/>", {
