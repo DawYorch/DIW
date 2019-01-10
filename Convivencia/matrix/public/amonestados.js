ab19aa3ae06d0f6d7c671a443d50f80b9488a9a1
DawYorch <yorch100976@gmail.com>
2019-01-10 19:01:21 +0100

    Mas cambios

--- Convivencia/matrix/public/amonestados.js
+++ Convivencia/matrix/public/amonestados.js
@@ -1,9 +1,54 @@
 
+var seleccion;
+var arrayVaciar;
+
+// MODO RAFA 
+
+// function cambio(){
+
+//     seleccion = document.getElementById("alumnos").value;
+//     // console.log(seleccion);
+    
+//     vaciado();
+//     document.getElementById(seleccion).style.order=-1;
+    
+
+// }
+
+// function vaciado(){
+
+//     arrayVaciar = document.getElementsByClassName("alumno");
+
+//     for(i=0 ; i<arrayVaciar.length ; i++){
+
+//         arrayVaciar[i].style.order=0;
+//     }
+
+// }
+
+
+// MODO ADRIAN
+
+
+var anterior;
+
 function cambio(){
 
-    var seleccion = document.getElementById("alumnos").value;
-    console.log(seleccion);
+    if(anterior != null){
+
+        document.getElementById(anterior).style.order=0;
+
+    }
+
+    seleccion = document.getElementById("alumnos").value;
+    document.getElementById(seleccion).style.order=-1;
+
+    anterior = seleccion;
 
 }
 
 
+
+    
+// Como hemos puesto la funcion en el mismo select (onchange) no ponemos document.addEventListener("DOMContentLoaded" , cargar , false);
+// y dentro de la funcion cargar pondriamos document.getElementById("alumnos")addEventListener("change" , cambio, false);
